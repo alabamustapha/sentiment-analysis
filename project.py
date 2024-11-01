8122aeb4aa768143b876175f6d65b9d2e114caff
Mustapha Alaba <alabamustapha@gmail.com>
2024-11-01 12:41:41 +0200

    project file added

---
+++ project.py
@@ -0,0 +1,17 @@
+# list of african countries 
+
+# scrape the wiki content for each country to a file 
+
+# for each country get sumary of sentiment and put in a dictionary
+
+# plot the polarity bargraph for each country
+
+# plot the subjectivity bargraph for each country
+
+# plot polarity against subjectivity for each country
+
+# get frequency of words in the summary of each country
+
+# get the most common words accross all countries
+
+# search for key words - war, poverty, corruption, insecurity, crime
